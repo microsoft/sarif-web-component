ffdf0c1a7c8b09318a5a8becfd2fb71bb1aa7a9c
Jeff King <jeffking@gmail.com>
2019-01-17 15:39:45 -0800

    Fix: Allow undefined for sarif prop.

--- components/ResultsViewer.tsx
+++ components/ResultsViewer.tsx
@@ -18,7 +18,7 @@ import {ResultsList} from './ResultsList.tsx'
 	}
 	UNSAFE_componentWillReceiveProps(nextProps) {
 		this.store.needSyncSel = true
-		this.store.results = nextProps.sarif === '' ? [] : parse(nextProps.sarif)
+		this.store.results = nextProps.sarif && parse(nextProps.sarif) || []
 		this.store.prefix = nextProps.prefix
 		return null
 	}
