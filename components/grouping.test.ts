ed7fef75eb90db7111fb2a93ac3467959ebb7142
Jeff King <jeffking@gmail.com>
2019-03-20 17:51:17 -0700

    Fix test.

--- components/grouping.test.ts
+++ components/grouping.test.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-import {generateGroups, sortItems, generateItems} from './grouping.ts'
+import {generateGroups, sortItems, generateItems} from './grouping'
 
 test('groups', () => {
 	const results = [
