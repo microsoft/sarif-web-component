a917f2076842ce3c7778cbd183aba3a2498cbdd2
Jeff King <jeffking@gmail.com>
2019-12-18 14:54:54 -0800

    Add resources folder.

---
+++ resources/sample.ts
@@ -0,0 +1,213 @@
+import {Log, Run, ReportingDescriptor, Result} from 'sarif'
+
+export default [{
+	version: "2.1.0",
+	runs: [{
+		tool: {
+			driver: {
+				name: "Sample Tool",
+				rules: [
+					{
+						id: 'RuleId 1',
+						name: 'Rule 1 Name',
+						helpUri: 'https://github.com/Microsoft/sarif-sdk',
+						fullDescription: { text: 'Full description for RuleId 1. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
+						relationships: [
+							{
+								target: {
+									id: '??',
+									index: 0,
+									toolComponent: { index: 0 }
+								}
+							},
+						],
+					},
+				]
+			},
+		},
+		taxonomies: [
+			{
+				taxa: [
+					{
+						name: 'Taxon00',
+						helpUri: 'Taxon00/help/uri',
+					},
+				]
+			}
+		],
+		results: [
+			// { // Bare minimum
+			//     message: {},
+			// },
+			{
+				ruleId: 'RuleId 1',
+				message: { text: 'Message only. Keyword "location" for testing.' },
+				locations: [{
+					physicalLocation: { artifactLocation: { uri: 'folder/file3.txt' } },
+				}],
+				baselineState: 'absent',
+				workItemUris: ['http://sarifviewer.azurewebsites.net/'],
+			},
+			{
+				ruleId: 'RuleId 1/1',
+				message: { text: 'Message with basic snippet.' },
+				locations: [{
+					physicalLocation: {
+						artifactLocation: { uri: 'folder/file1.txt' },
+						region: {
+							snippet: { text: 'Region snippet text only abc\n'.repeat(10) },
+						},
+					}
+				}],
+				baselineState: 'new',
+				level: 'error',
+			},
+			{
+				ruleId: 'RuleId 1',
+				message: {},
+				locations: [{
+					physicalLocation: {
+						artifactLocation: { uri: 'folder/file2.txt' },
+						region: {
+							snippet: { text: 'Region snippet text only. No message. def' },
+						},
+					}
+				}],
+				baselineState: 'unchanged',
+				level: 'note',
+			},
+			{
+				ruleId: 'RuleId 1',
+				message: { text: 'Testing show all.' },
+				locations: [{
+					physicalLocation: { artifactLocation: { uri: 'folder/file4.txt' } },
+				}],
+				baselineState: 'new',
+			},
+
+			// Variation in Path.
+			{
+				ruleId: 'RuleId 2',
+				message: { text: 'No path.' },
+				baselineState: 'updated',
+				level: 'none',
+			},
+			{
+				ruleId: 'RuleId 2',
+				message: { text: 'Only analysisTarget.' },
+				analysisTarget: { uri: 'analysisTarget' }
+			},
+			{
+				ruleId: 'RuleId 2',
+				message: {
+					markdown: "Fix any of the following:\n- Element does not have an alt attribute\n- aria-label attribute does not exist or is empty\n- aria-labelledby attribute does not exist, references elements that do not exist or references elements that are empty\n- Element has no title attribute or the title attribute is empty\n- Element's default semantics were not overridden with role=\"presentation\"\n- Element's default semantics were not overridden with role=\"none\".",
+					text: 'Only analysisTarget.',
+				},
+				analysisTarget: { uri: 'analysisTarget' }
+			},
+
+			// Variations in Snippets.
+			{
+				ruleId: 'RuleId 3',
+				message: { text: '1. Message with basic snippet and startLine' },
+				locations: [{
+					physicalLocation: {
+						artifactLocation: { uri: 'folder/file.txt' },
+						region: {
+							snippet: { text: 'Region snippet text only.' },
+							startLine: 100,
+						},
+					}
+				}]
+			},
+			{
+				ruleId: 'RuleId 3',
+				message: { text: '2. Message with basic snippet and contextRegion' },
+				locations: [{
+					physicalLocation: {
+						artifactLocation: { uri: 'folder/file.txt' },
+						region: {
+							snippet: { text: 'Region snippet text only.' },
+							startLine: 100,
+						},
+						contextRegion: {
+							snippet: { text: 'aaa\nRegion snippet text only.\nbbb' },
+						},
+					}
+				}]
+			},
+
+			// Variations in AXE.
+			{
+				ruleId: 'RuleId 4',
+				message: { text: '1. AXE-ish location. Typical.' },
+				locations: [{
+					logicalLocation: { fullyQualifiedName: 'fullyQualifiedName' },
+					physicalLocation: {
+						artifactLocation: { index: 0 }, // Link to...
+						region: {
+							snippet: { text: 'Region snippet text only' },
+						},
+					}
+				}]
+			},
+			{
+				ruleId: 'RuleId 4',
+				message: { text: '2. AXE-ish location. No artifact location.' },
+				locations: [{
+					logicalLocation: { fullyQualifiedName: 'fullyQualifiedName' },
+					physicalLocation: {
+						region: {
+							snippet: { text: 'Region snippet text only' },
+						},
+					}
+				}]
+			},
+			{
+				ruleId: 'RuleId 4',
+				message: { text: '1. AXE-ish location. Typical.' },
+				locations: [{
+					physicalLocation: {
+						artifactLocation: { uri: 'fallback-missing-fullyQualifiedName' },
+						region: {
+							snippet: { text: 'Region snippet text only' },
+						},
+					}
+				}]
+			},
+		],
+		artifacts: [
+			{
+				location: { uri: 'indexed/artifact/uri' },
+				description: { text: 'Some really long text for indexed/artifact/uri' },
+			},
+		]
+	}],
+},
+{
+	version: "2.1.0",
+	runs: [{
+		tool: {
+			driver: {
+				name: "Sample Tool 2"
+			}
+		},
+		results: [
+			{ // Bare minimum
+				message: {},
+			},
+		],
+	},
+	{
+		tool: {
+			driver: {
+				name: "Sample Tool 3"
+			}
+		},
+		results: [
+			{ // Bare minimum
+				message: {},
+			},
+		],
+	}]
+}] as Log[]
