1ef21228b31fccd4e06d1f680587c12529177d6c
Jeff King <jeffking@gmail.com>
2019-12-17 16:28:31 -0800

    Reset repo.

--- Index.tsx
+++
@@ -1,5 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-export {ResultsViewer} from './components/ResultsViewer'
-export {Dropdown} from 'office-ui-fabric-react/lib/Dropdown'
--- jestSetup.js
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-const enzyme = require("enzyme")
-const Adapter = require("enzyme-adapter-react-16")
-
-enzyme.configure({ adapter: new Adapter() })
