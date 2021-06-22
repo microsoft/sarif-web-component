c21895e37ee6d5eb3981b44aec62af32591ee735
Jeff King <jeffking@gmail.com>
2021-06-22 12:18:12 -0700

    Jest config maxWorkers.

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+	// 10x perf improvement, see:
+	// https://github.com/kulshekhar/ts-jest/issues/1044
+	// Consider ts-jest isolatedModules for more perf.
+	maxWorkers: 1,
+
 	// testEnvironment: 'node',
 	moduleNameMapper: {
 		'\\.(png|s?css)$': 'identity-obj-proxy'
